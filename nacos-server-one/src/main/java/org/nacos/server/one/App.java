ed13e9cd1634b839f91b59decd7348d2b448a6c3
WTG2010 <qianchuan2010@outlook.com>
2020-08-05 16:04:21 +0800

    服务模块调整

--- nacos-server-one/src/main/java/org/nacos/server/one/App.java
+++ nacos-server-one/src/main/java/org/nacos/server/one/App.java
@@ -2,14 +2,8 @@ package org.nacos.server.one;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Hello world!
- *
- */
 @SpringBootApplication
-@RestController
 public class App 
 {
     public static void main( String[] args )
