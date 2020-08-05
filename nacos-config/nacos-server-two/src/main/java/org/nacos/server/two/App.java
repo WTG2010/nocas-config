55544c11fb328653574b59aac56ed77aaad99d9c
WTG2010 <qianchuan2010@outlook.com>
2020-08-05 15:59:01 +0800

    maven 父级项目提交

---
+++ nacos-config/nacos-server-two/src/main/java/org/nacos/server/two/App.java
@@ -0,0 +1,25 @@
+package org.nacos.server.two;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class App 
+{
+    public static void main( String[] args )
+    {
+       SpringApplication.run(App.class, args);
+    }
+    
+    @Value("${common}")
+    String common;
+    
+    @GetMapping("getCommon")
+    public String getCommon() {
+		return common;
+	}
+}
---
+++ nacos-server-one/src/main/java/org/nacos/server/one/App.java
@@ -0,0 +1,19 @@
+package org.nacos.server.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@RestController
+public class App 
+{
+    public static void main( String[] args )
+    {
+       SpringApplication.run(App.class, args);
+    }
+}
